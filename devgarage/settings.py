63ad43655783262991959b32cb5bac2a52910a5c
Nilay <52445556+nilaycicekli@users.noreply.github.com>
2021-02-19 23:16:14 +0300

    Update settings.py
    credentials removed

--- devgarage/settings.py
+++ devgarage/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '%h+m*^mzavyqx_1sbvh-+3#b86$ob^)_muvzb_ly2hg7)$g&i@'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -141,8 +141,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "dev.garaj@gmail.com"
-EMAIL_HOST_PASSWORD = "devgarage1234"
+EMAIL_HOST_USER = ""
+EMAIL_HOST_PASSWORD = ""
 
 CKEDITOR_CONFIGS = {
     'default':
